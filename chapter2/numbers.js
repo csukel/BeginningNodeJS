0f9a5f2c499c258e4c6f55fac41d308c7bb160be
Loucas Stylianou <loukkis91@gmail.com>
2016-02-07 11:13:31 +0200

    initial commit

---
+++ chapter2/numbers.js
@@ -0,0 +1,10 @@
+/**
+ * Created by loukk on 07/02/2016.
+ */
+var foo = 1;
+var bar = 3;
+console.log(foo *5);//5
+console.log(foo - bar);//-2
+console.log(foo/bar);//0.33
+console.log(foo*bar);//3
+console.log(foo % 2);// remainder 1
---
+++ chapter2/variable.js
@@ -0,0 +1,7 @@
+/**
+ * Created by loukk on 07/02/2016.
+ */
+
+
+var foo = 123;
+console.log(foo);
